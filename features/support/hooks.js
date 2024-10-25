6d3687ca3de43c38e5d4500a2728b0cce43419b2
Smita Ramakrishna Shetty <smitaramakrishna.10821339@ltimindtree.com>
2024-10-25 12:45:28 +0530

    We have update the code for getting HTML report,video,trace for the test run

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,33 +1,63 @@
 const {POMmanager}=require('../../POM-Manager/POMmanager');
 const playwright= require('@playwright/test');
-const {After, Before, Status,AfterStep, BeforeStep }= require('@cucumber/cucumber');
+const reporter = require('cucumber-html-reporter');
+const {After, Before, Status,AfterStep, BeforeStep,AfterAll }= require('@cucumber/cucumber');
+const { type } = require('os');
 
 
-Before(async function () {
-     this.browser= await playwright.chromium.launch({
-        headless:false
+Before(async function ({pickle}) {
+   this.browser = await playwright.chromium.launch({
+      headless: false
     });
-    const context = await this.browser.newContext();
-     this.page = await context.newPage();
+  
+    this.context = await this.browser.newContext({
+      recordVideo: {
+         dir: './test-results/videos/',
+      }
+    });
+  
+    this.page = await this.context.newPage();
+    await this.context.tracing.start({ screenshots: true, snapshots: true });
+
+    
   });
 
-  After(async function () {
+  After(async function ({pickle}) {
 
-   // await this.page.close();
+    await this.context.tracing.stop({ path: `./test-results/traces/${pickle.name}.zip`, type:"zip" });
+    await this.context.close();
     await this.browser.close();
     console.log("Here the browser is closed succesfully")
     
   })
 
-  BeforeStep({tags: ""}, function () {
-    // This hook will be executed before all steps in a scenario with tag @regression
-  });
+  AfterStep( async function ({result,pickle}) {
   
-  AfterStep( async function ({result}) {
-  
-    if (result.status === Status.FAILED) 
+    if (result.status === Status.PASSED) 
      {
-        await this.page.screenshot({path: 'FailedTestScreenshot.png'});
+       const img= await this.page.screenshot({path: `./test-results/screenshots/${pickle.name}.png`, type:"png"}); // we are getting the screenshot here
+       await this.attach(img,"image/png");
      }
 
-  });
+
+   AfterAll(async function () {
+      const options = {
+        theme: 'bootstrap',
+        jsonFile: './test-results/reports/cucumber_report.json',
+        output: './test-results/reports/cucumber_report.html',
+        reportSuiteAsScenarios: true,
+        launchReport: true,
+        metadata: {
+          "App Version": "0.3.2",
+          "Test Environment": "STAGING",
+          "Browser": "Chrome  54.0.2840.98",
+          "Platform": "Windows 10",
+          "Parallel": "Scenarios",
+          "Executed": "Remote"
+        }
+      }
+      reporter.generate(options);
+});
+
+
+});
